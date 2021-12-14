579a579aaec2a6e837d1494e179d04dde1e3a9c0
ThankGod Nnaji - Ilvestech <decentlevi@gmail.com>
2021-12-14 12:04:10 -0500

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,122 @@
+
+public class Account {
+
+    public static Account account;
+
+    private static int balance = 10000;
+
+    private static Person person;
+
+
+
+    private Account() {
+
+    }
+
+
+
+    public static Account getAccount(Person p) {
+
+        if (account == null) {
+
+            account = new Account();
+
+        }
+
+        Account.person = p;
+
+        return account;
+
+    }
+
+
+
+    public static int getBal() {
+
+        return balance;
+
+    }
+
+
+
+    public synchronized void withdraw(int bal) {
+
+        try {
+
+
+
+            if (balance >= bal) {
+
+                System.out.println(person.getName() + " " + "is trying to withdraw");
+
+                try {
+
+                    Thread.sleep(500);
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+
+                }
+
+                balance = balance - bal;
+
+                System.out.println(person.getName() + " " + "completed the withdraw");
+
+            } else {
+
+                System.out.println(person.getName() + " " + "doesn't have enough money to withdraw ");
+
+            }
+
+            System.out.println(person.getName() + " " + " withdrew $" + bal + " Current balance: $"+ balance);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+
+
+    public synchronized void deposit(int bal) {
+
+        try {
+
+            if (bal > 0) {
+
+                System.out.println(person.getName() + " " + " is trying to deposit");
+
+                try {
+
+                    Thread.sleep(200);
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+
+                }
+
+                balance = balance + bal;
+
+                System.out.println(person.getName() + " " + " completed the deposit");
+
+            } else {
+
+                System.out.println(person.getName() + " " + "doesn't have enough money to deposit");
+
+            }
+
+            System.out.println(person.getName() + " " + " deposited $" + bal + " Current balance: $" + balance);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
